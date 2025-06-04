e8baf4e08cab80753e11b4d77eaddfa35c56f44a
FAANG-CODES <naveen.theegala88@gmail.com>
2025-06-04 16:46:07 +0530

    Add solution for Move Zeroes

--- LeetCode/Move-Zeroes/Solution.java
+++ LeetCode/Move-Zeroes/Solution.java
@@ -1,16 +1,12 @@
-class Solution(object):
-    def moveZeroes(self, nums):
-        left =0
-        for right in range(len(nums)):
-            if nums[right]!=0:
-                nums[left],nums[right] = nums[right],nums[left]
-                left+=1
-
-
-
-
-        """
-        :type nums: List[int]
-        :rtype: None Do not return anything, modify nums in-place instead.
-        """
-        
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int left = 0;
+        for(int right = 0; right < nums.size();right++){
+            if (nums[right] != 0){
+                swap(nums[left], nums[right]);
+                left++;
+            }
+        }
+    }
+};
